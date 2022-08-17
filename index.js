4eaec98c52db06f2132d5b1edd16a1a91de57558
Kassidy Tavel <110184944+ktavel@users.noreply.github.com>
2022-08-17 11:29:58 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+newArray = ages = [3, 9, 23, 64, 2, 8, 28, 93];
+console.log(ages);
+//printing the subtraction value of the frist element from the last element in the array
+
+console.log(ages[ages.length - 1] -ages[0]);
